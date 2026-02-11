68fc1600426933cc36c3dce9c9a64ddc5c81ad3c
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-02-11 15:02:38 +0700

    memperbaiki performance

--- card.js
+++ card.js
@@ -14,33 +14,38 @@ document.querySelectorAll(".galeri-card").forEach((card) => {
     card.classList.toggle("aktif");
 
     if (card.classList.contains("aktif")) {
-      // jadi sejajar
-      imgs[0].style.left = "-1.5rem";
+      // sejajar
+      imgs[0].style.left = "0.5rem";
       imgs[0].style.top = "0";
+      imgs[0].style.zIndex = "10";
 
       imgs[1].style.left = "4rem";
       imgs[1].style.top = "0";
+      imgs[1].style.zIndex = "20";
 
-      imgs[2].style.left = "8rem";
+      imgs[2].style.left = "6rem";
       imgs[2].style.top = "0";
+      imgs[2].style.zIndex = "30";
     } else {
-      // balik stack
-      imgs[0].style.left = "0";
+      // stack
+      imgs[0].style.left = "1.5rem";
       imgs[0].style.top = "1rem";
+      imgs[0].style.zIndex = "30";
 
       imgs[1].style.left = "2rem";
       imgs[1].style.top = "0";
+      imgs[1].style.zIndex = "20";
 
       imgs[2].style.left = "4rem";
       imgs[2].style.top = "2rem";
+      imgs[2].style.zIndex = "10";
     }
   });
 });
-
-const btn = document.getElementById("menu-btn");
+const menubtn = document.getElementById("menu-btn");
 const menu = document.getElementById("mobile-menu");
 
-btn.addEventListener("click", () => {
+menubtn.addEventListener("click", () => {
   menu.classList.toggle("max-h-0");
   menu.classList.toggle("opacity-0");
   menu.classList.toggle("max-h-screen");
